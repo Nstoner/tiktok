c3f1e7c3f1119609ffcce0560f0221904a6bf227
Nstoner <hsuw850@gmail.com>
2022-12-27 11:29:42 +0630

    All Fixed

--- script.js
+++ script.js
@@ -10,33 +10,19 @@ hourAndMinutes.textContent = horusText + ":" + minutesText;
 
 const videoTag = document.querySelectorAll(".videoTag");
 const progressBar = document.getElementsByClassName("progress-bar")[0];
-const currentProgress = document.getElementsByClassName("current-progress")[0];
+const currentProgress = document.querySelector(".current-progress");
 const sliderTag = document.querySelectorAll(".slider");
 const homeContainer = document.getElementsByClassName("home-container")[0];
 
 
 
 
+
+
+
 let duration = 0;
-videoTag.forEach((video) => video.addEventListener("loadeddeta", () => {
-  duration = Math.floor(video.duration);
-}))
-
-videoTag.forEach((video) => video.addEventListener("timeupdate", () => {
-  const currentTime = Math.floor(videoTag.currentTime);
-  updateCurrentProgress(currentTime);
-}))
-
-const updateCurrentProgress = (currentTime) => {
-  const currentProgressWidth = (100/duration) * currentTime;
-  currentProgress.style.width = currentProgressWidth.toString() + "%";
-};
-
-
-  
-
-
 const handleClick = (video) => {
+  
   videoTag.forEach((ev) => ev.pause())
   if (video.target.classList.contains("active")) {
     video.target.classList.remove("active");
@@ -45,9 +31,24 @@ const handleClick = (video) => {
   }else {
     video.target.classList.add("active");
     video.target.play()
+  };
+  video.target.addEventListener("loadeddeta", () => {
+    const duration = Math.floor(video.duration);
+  });
+  video.target.addEventListener("timeupdate", () => {
+    const currentTime = Math.floor(video.target.currentTime);
+    const currentTimeText = currentTime.toString();
+    updateCurrentSecond(currentTime)
+  });
+
+  const updateCurrentSecond = (currentTime) => {
+    const currentProgressWidth = (100/video.target.duration) * currentTime;
+    currentProgress.style.width = currentProgressWidth.toString() + "%";
   }
   
 }
+
+
 
 sliderTag.forEach((slider) => slider.addEventListener("click", handleClick));
 
